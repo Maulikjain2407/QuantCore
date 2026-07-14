04d5e1841474df6d62b2157b7b3fa34b56cefbfa
Maulikjain2407 <maulikjain2407@icloud.com>
2026-07-15 03:11:05 +0530

    UPDATE[ADDITION]
    addition of new metrics such as sortino ratio,calmar ratio, recovery factor and CAGR

--- backtest/metrics.py
+++ backtest/metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
 from configs.config import HOLDING_PERIOD
+
 def cumulative_returns(strategy_returns):
     cumulative=(1+strategy_returns).cumprod()
 
@@ -26,29 +27,88 @@ def max_drawdown(strategy_returns):
 
     return drawdown.min()
 
-def calculate_metrics(
-    backtest_df,
-    holding_period=HOLDING_PERIOD
-):
+def sortino_ratio(strategy_returns,holding_period=HOLDING_PERIOD):
+    
+    mean_return=strategy_returns.mean()
 
-    strategy_returns = backtest_df[
-        "strategy_return"
-    ]
+    downside_returns= strategy_returns[strategy_returns<0]
 
-    return {
+    downside_std=downside_returns.std()
+
+    if downside_std==0:
+        return 0
+
+    annualisation_factor=np.sqrt(252/holding_period)
+
+    sortino=annualisation_factor*(mean_return/downside_std)
+
+    return sortino
+    
+def calmar_ratio(strategy_returns,holding_period=HOLDING_PERIOD):
+
+    cumulative=(1+strategy_returns).prod()
+
+    years = (len(strategy_returns)*holding_period) / 252
+    annual_return=(cumulative**(1/years))-1
+
+    maximum_drawdown=abs(max_drawdown(strategy_returns))
+
+    if maximum_drawdown==0:
+        return 0
+    
+    calmar=annual_return/maximum_drawdown
+
+    return calmar
+
+def recovery_factor(strategy_returns):
+    total_returns=cumulative_returns(strategy_returns)
+
+    maximum_drawdown=abs(max_drawdown(strategy_returns))
+
+    if maximum_drawdown==0:
+        return 0
+    
+    recover= total_returns/maximum_drawdown
+
+    return recover
+
+def CAGR(strategy_returns,holding_period=HOLDING_PERIOD):
+
+    cumulative=(1+strategy_returns).prod()
+
+    years=(len(strategy_returns)*holding_period)/252
+
+    if years==0:
+        return 0
+    
+    cagr=(cumulative**(1/years))-1
+
+    return cagr
+
+def calculate_metrics(backtest_df,holding_period=HOLDING_PERIOD):
+
+    strategy_returns = backtest_df["strategy_return"]
+
+    return {    
+
         "cumulative_return":
-            cumulative_returns(
-                strategy_returns
-            ),
+            cumulative_returns(strategy_returns),
 
         "sharpe_ratio":
-            sharpe_ratio(
-                strategy_returns,
-                holding_period
-            ),
+            sharpe_ratio(strategy_returns,holding_period),
+
+        "sortino_ratio":
+            sortino_ratio(strategy_returns,holding_period),
 
         "max_drawdown":
-            max_drawdown(
-                strategy_returns
-            )
+            max_drawdown(strategy_returns),
+
+        "calmar_ratio":
+            calmar_ratio(strategy_returns,holding_period),
+
+        "recovery_factor":
+            recovery_factor(strategy_returns),
+
+        "cagr":
+            CAGR(strategy_returns,holding_period)
     }
