4fdec8085d62cc88d05f59f199f64550bb8ba86a
Maulikjain2407 <maulikjain2407@icloud.com>
2026-07-14 22:11:19 +0530

    Update[model_pipeline]
    Updation of model_pipeline from previous binary label and feature engineering to new triple barrier label

--- pipelines/model_pipeline.py
+++ pipelines/model_pipeline.py
@@ -1,8 +1,9 @@
 from src.data_loader import get_data
 from src.data_cleaner import clean_data
-from feature_engineer.feature_engineering import create_feature
-from label_creator.label_creator import create_label
+from feature_engineer.feature_engineering_V2 import create_feature
+from label_creator.triple_barrier_labeling import create_triple_barrier_label
 from src.test_train_split import test_train_split
+from configs.config import STOP_LOSS_MULTIPLIER,TAKE_PROFIT_MULTIPLER,HOLDING_PERIOD
 
 from models.model import train_model
 from utils.model_io import save_model
@@ -12,7 +13,7 @@ def run_train_pipeline():
     get_data()
     clean_data()
     create_feature()
-    create_label()
+    create_triple_barrier_label(holding_period=HOLDING_PERIOD,take_profit_multiplier=TAKE_PROFIT_MULTIPLER,stop_loss_multiplier=STOP_LOSS_MULTIPLIER)
     x_train,y_train,x_test,y_test,test_df=test_train_split()
     
     model=train_model(x_train=x_train,y_train=y_train)
